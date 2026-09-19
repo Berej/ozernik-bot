fde467938ac38cd8e03995c83dacc0b1e8362a29
Berej <tbm6040@gmail.com>
2026-09-19 17:41:08 +0300

    int type error in set

--- modules/rsn/rsn.py
+++ modules/rsn/rsn.py
@@ -231,8 +231,8 @@ class RsnCog(commands.Cog):
 
     async def _check_permissions(self, interaction: discord.Interaction) -> bool:
         """Проверить, есть ли у пользователя права админа/модератора (по ролям)."""
-        admin_role_ids = 725675581881974794
-        mod_role_ids = 779015800555176006
+        admin_role_ids = "725675581881974794"
+        mod_role_ids = "779015800555176006"
         
         if not isinstance(interaction.user, discord.Member):
             await interaction.response.send_message(
